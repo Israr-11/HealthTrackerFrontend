81edfe9e2a82b6b60b5cdc78a1d4ca39078c7c27
Israr Ahmed <israrahmedpk1999@gmail.com>
2024-12-25 15:29:00 +0000

    Feature 7: Mental Health Completed

---
+++ src/components/mentalhealth/LogList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-between align-items-center mb-1 mt-5">
+      <h3 class="table-title">Mental Health Logs</h3>
+    </div>
+
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>#</th>
+        <th>Mental Health Goal ID</th>
+        <th>Mood score (out of 10)</th>
+        <th>Stress Level (out of 10)</th>
+        <th>Target Achieved</th>
+        <th>Recommendations</th>
+        <th>Entry Time</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(log, index) in logs" :key="log.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ log.mentalHealthGoalId }}</td>
+        <td>{{ log.moodScore}}</td>
+        <td>{{ log.stressLevel }}</td>
+        <td>{{ log.targetMet? 'Yes' : 'No' }}</td>
+        <td>{{ log.recommendations}}</td>
+        <td>{{ new Date(log.entryTime).toLocaleString() }}</td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import {
+  getMentalHealthLogsByUserId,
+} from "../../services/mentalhealth.js";
+
+export default {
+  name: "LogList",
+  components: {
+  },
+  data() {
+    return {
+      logs: [],
+    };
+  },
+  async mounted() {
+    await this.fetchLogs();
+  },
+  methods: {
+    async fetchLogs() {
+      const user = JSON.parse(localStorage.getItem("user"));
+      const userId = user.id;
+      try {
+        const response = await getMentalHealthLogsByUserId(userId);
+        this.logs = response.data;
+      } catch (error) {
+        console.error("Error fetching logs:", error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.d-flex {
+  display: flex;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+.align-items-center {
+  align-items: center;
+}
+.table-title {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.table {
+  margin-top: 20px;
+}
+.btn-primary {
+  background-color: #5c636a;
+  border-color: #5c636a;
+  transition: all 0.3s ease;
+}
+</style>
