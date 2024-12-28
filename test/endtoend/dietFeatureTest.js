560ce4af7762f651a137201493ddc31806b903b3
Israr Ahmed <91403838+Israr-11@users.noreply.github.com>
2024-12-28 19:59:14 +0000

    Modification in dietFeatureTest.js

--- test/endtoend/dietFeatureTest.js
+++ test/endtoend/dietFeatureTest.js
@@ -1,5 +1,6 @@
-import { Builder, By, until } from 'selenium-webdriver';
+import { Builder, By, Key, until } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
+import assert from 'assert';
 
 describe('Diet Feature Test', function() {
     this.timeout(30000)
@@ -12,7 +13,8 @@ describe('Diet Feature Test', function() {
         options.addArguments('--no-sandbox');
         options.addArguments('--disable-dev-shm-usage');
         options.addArguments('--disable-gpu');
-        options.addArguments('--window-size=1552,840');
+        options.addArguments('--window-size=1920,1080');
+        options.addArguments('--start-maximized');
 
         driver = await new Builder()
             .forBrowser('chrome')
@@ -27,30 +29,48 @@ describe('Diet Feature Test', function() {
 
     it('Diet Feature Test', async function() {
         await driver.get("https://health-tracker-application.netlify.app/")
-        await driver.manage().window().setRect({ width: 1552, height: 840 })
+        await driver.executeScript("window.scrollTo(0, 0)");
 
+        await driver.wait(until.elementLocated(By.id("fullName")), 10000);
         await driver.findElement(By.id("fullName")).sendKeys("Israr")
         await driver.findElement(By.id("email")).sendKeys("israr@gmail.com")
-        await driver.findElement(By.css(".btn")).click()
+        
+        const loginButton = await driver.findElement(By.css(".btn"))
+        await driver.executeScript("arguments[0].scrollIntoView(true);", loginButton);
+        await driver.wait(until.elementIsVisible(loginButton), 10000);
+        await loginButton.click()
 
         const dietButton = await driver.wait(
             until.elementLocated(By.linkText("Go to Diet Tracking")),
             10000
         );
+        await driver.executeScript("arguments[0].scrollIntoView(true);", dietButton);
+        await driver.wait(until.elementIsVisible(dietButton), 10000);
         await dietButton.click();
 
-        await driver.findElement(By.css(".btn:nth-child(2)")).click()
-        await driver.wait(until.elementLocated(By.id("dietGoalType")), 10000)
+        const createButton = await driver.wait(
+            until.elementLocated(By.css(".btn:nth-child(2)")),
+            10000
+        );
+        await driver.executeScript("arguments[0].scrollIntoView(true);", createButton);
+        await driver.wait(until.elementIsVisible(createButton), 10000);
+        await createButton.click();
 
-        const dietSelect = await driver.findElement(By.id("dietGoalType"))
-        await dietSelect.click()
-        await dietSelect.findElement(By.css("option[value='vegetarian']")).click()
+        await driver.wait(until.elementLocated(By.id("dietGoalType")), 10000);
+        const dietSelect = await driver.findElement(By.id("dietGoalType"));
+        await driver.executeScript("arguments[0].scrollIntoView(true);", dietSelect);
+        await dietSelect.click();
+        await dietSelect.findElement(By.css("option[value='vegetarian']")).click();
 
-        await driver.findElement(By.id("targetCalories")).sendKeys("2000")
+        const caloriesInput = await driver.findElement(By.id("targetCalories"));
+        await driver.executeScript("arguments[0].scrollIntoView(true);", caloriesInput);
+        await caloriesInput.sendKeys("2000");
 
-        const submitButton = await driver.findElement(By.css(".modal-footer > .btn-primary"))
-        await submitButton.click()
+        const submitButton = await driver.findElement(By.css(".modal-footer > .btn-primary"));
+        await driver.executeScript("arguments[0].scrollIntoView(true);", submitButton);
+        await driver.wait(until.elementIsVisible(submitButton), 10000);
+        await submitButton.click();
 
-        await driver.sleep(2000)
+        await driver.sleep(2000);
     })
 })
